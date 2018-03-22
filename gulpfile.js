68cce60b110cec2e0f3610872eaafc55e9a9416a
Abdullah <hostmaker@yahoo.com>
2018-03-22 10:19:14 +0600

    Add Css style of Large Hero section

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+postcssSimpleVars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssimport = require('postcss-import');
 
 gulp.task('default', function(){
     console.log('Horray...We have just created a gulp project');
@@ -11,7 +16,10 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles',function(){
-   console.log('Sass or PostCss work is going');
+   //console.log('Sass or PostCss work is going');
+   return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssimport,postcssSimpleVars,nested,autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
